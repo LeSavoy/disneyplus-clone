91ffa7ce204cb1f43fb72f70a8cb74009b8a918d
LeSavoy <mateus-santos-2011@live.com>
2022-07-25 22:36:36 -0300

    fetch content from data.json

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+import { getHomeContent } from "./services/getHomeContent.js";
+import Home from "./pages/home.js"
+
+getHomeContent().then((data) => {
+  Home(data)
+}).catch((error) => {
+
+})
---
+++ assets/js/components/user-profile.js
@@ -0,0 +1,10 @@
+const UserProfile = props => {
+  return `<li class="user-menu__item" data-id="${props.id}">
+            <div class="user-profile user-profile">
+              <img src="${props.avatar}" alt="" class="user-profile__avatar">
+              <span class="user-profile__title">${props.name}</span>
+            </div>
+          </li>`
+}
+
+export default UserProfile
